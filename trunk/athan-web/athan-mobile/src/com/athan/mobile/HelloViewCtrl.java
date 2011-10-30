2fcad4df22a6aef8a2365cf9e8f28c0a1b42117c
Makavelikal@gmail.com <Makavelikal@gmail.com@7c86f1f7-d457-8aa9-56d4-cee53fa95fa2>
2011-10-30 21:50:55 +0000

    Fichiers ressources (toujours en cours de test)

--- trunk/athan-web/athan-mobile/src/com/athan/mobile/HelloViewCtrl.java
+++ trunk/athan-web/athan-mobile/src/com/athan/mobile/HelloViewCtrl.java
@@ -1,6 +1,12 @@
 package com.athan.mobile;
 
+import java.util.Locale;
+
+import org.zkoss.util.Locales;
+import org.zkoss.util.resource.Labels;
+import org.zkoss.web.Attributes;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Button;
@@ -16,7 +22,8 @@ public class HelloViewCtrl extends GenericForwardComposer {
 	private static final long serialVersionUID = 1L;
 
 	private Label lblGreetings;
-	private Button btnHi;
+	private Button btnFR;
+	private Button btnEN;
 
 	private int i = 0;
 
@@ -31,12 +38,40 @@ public class HelloViewCtrl extends GenericForwardComposer {
 		Html html = new Html();
 		html.setContent("<div id=\"saad\">Hello !!</div>");
 		html.setParent(comp);
+		
+//		Labels.reset();
+		
+		Clients.alert("1. " + Locales.getCurrent().getCountry());
+		Clients.alert("2. " + Locales.getCurrent().getLanguage());
+		Clients.alert("3. " + Labels.getLabel("monlabel"));
 
 	}
 
 	public void onClick$btnHi() {
 		lblGreetings.setValue("" + (i++));
 		Clients.evalJavaScript("ecrire(" + i + ")");
+	}
+	
+	public void onClick$btnEN() {
+		session.setAttribute(Attributes.PREFERRED_LOCALE, Locale.ENGLISH);
+		StringBuffer sb = new StringBuffer();
+		sb.append("\nA. " + Labels.getLabel("monlabel"));
+		//Labels.reset();
+//		sb.append("\nB. " + Labels.getLabel("monlabel"));
+		Executions.sendRedirect(""); //reload the same page
+		sb.append("\nC. " + Labels.getLabel("monlabel"));
+		Clients.alert(sb.toString());
+	}
+	
+	public void onClick$btnFR() {
+		session.setAttribute(Attributes.PREFERRED_LOCALE, new Locale("fr","FR"));
+		StringBuffer sb = new StringBuffer();
+		sb.append("\nA. " + Labels.getLabel("monlabel"));
+		//Labels.reset();
+//		sb.append("\nB. " + Labels.getLabel("monlabel"));
+		Executions.sendRedirect(""); //reload the same page
+		sb.append("\nC. " + Labels.getLabel("monlabel"));
+		Clients.alert(sb.toString());
 	}
 
 }
