4b0490d1383a298f750ae4e2e96921ad49571c74
Makavelikal@gmail.com <Makavelikal@gmail.com@7c86f1f7-d457-8aa9-56d4-cee53fa95fa2>
2011-10-30 23:04:50 +0000

    Système de cookies pour la sauvegarde de la langue de l'appli

---
+++ athan-web/athan-mobile/src/com/athan/mobile/init/CookieUtil.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.athan.mobile.init;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.zkoss.zk.ui.Executions;
+
+/**
+ * Générateur de cookie
+ * 
+ * @author Saad BENBOUZID
+ */
+public class CookieUtil {
+	
+	public static void setCookie(String name, String value) {
+		((HttpServletResponse) Executions.getCurrent().getNativeResponse())
+				.addCookie(new Cookie(name, value));
+	}
+
+	public static String getCookie(String name) {
+		Cookie[] cookies = ((HttpServletRequest) Executions.getCurrent()
+				.getNativeRequest()).getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals(name)) {
+					return cookie.getValue();
+				}
+			}
+		}
+		return null;
+	}
+}
