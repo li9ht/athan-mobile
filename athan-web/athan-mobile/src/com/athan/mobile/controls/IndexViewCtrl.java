64e994945c495d07bf5dc73a89f6dfe70d5df2bd
Makavelikal@gmail.com <Makavelikal@gmail.com@7c86f1f7-d457-8aa9-56d4-cee53fa95fa2>
2011-11-01 15:05:55 +0000

    Base de la page (encore en cours)

---
+++ athan-web/athan-mobile/src/com/athan/mobile/controls/IndexViewCtrl.java
@@ -0,0 +1,40 @@
+package com.athan.mobile.controls;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Button;
+
+import com.athan.mobile.init.CookieUtil;
+import com.athan.mobile.init.LocalesProvider;
+
+/**
+ * 
+ * @author BENBOUZID
+ */
+public class IndexViewCtrl extends GenericForwardComposer {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Button english_flag;
+	private Button french_flag;
+
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+
+		// TODO
+
+	}
+
+	public void onClick$english_flag() {
+		CookieUtil.setCookie(LocalesProvider.MY_LOCALE_COOKIE_NAME, LocalesProvider.LOCALE_EN);
+		Executions.sendRedirect(""); //reload the same page
+	}
+	
+	public void onClick$french_flag() {
+		CookieUtil.setCookie(LocalesProvider.MY_LOCALE_COOKIE_NAME, LocalesProvider.LOCALE_FR);
+		Executions.sendRedirect(""); //reload the same page
+	}
+
+}
