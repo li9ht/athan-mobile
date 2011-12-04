189130a25a00cbf5e5dbabce2553c6c4975b5c28
Makavelikal@gmail.com <Makavelikal@gmail.com@7c86f1f7-d457-8aa9-56d4-cee53fa95fa2>
2011-12-04 17:32:42 +0000

    Date et n° de version récupérées dans Athan.jad

--- trunk/athan-web/athan-mobile/src/com/athan/mobile/controls/download/CurrentViewCtrl.java
+++ trunk/athan-web/athan-mobile/src/com/athan/mobile/controls/download/CurrentViewCtrl.java
@@ -5,6 +5,10 @@ package com.athan.mobile.controls.download;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,8 +30,14 @@ public class CurrentViewCtrl extends GenericForwardComposer {
 
 	private static final long serialVersionUID = 1L;
 	
-	private static final Logger LOG = Logger.getLogger(CurrentViewCtrl.class.getName());
-	
+	private static final String MIDLET_VERSION = "MIDlet-Version";
+
+	private static final Logger LOG = Logger.getLogger(CurrentViewCtrl.class
+			.getName());
+
+	private Label lblCurrentVersion;
+	private Label lblCurrentDate;
+
 	private Label lblJadSize;
 	private Label lblJarSize;
 
@@ -35,21 +45,127 @@ public class CurrentViewCtrl extends GenericForwardComposer {
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
 
+		displayFileProperties();
+		
+		displayVersionProperties();
+	}
+
+	/**
+	 * Display version properties (version number, modification date,...)
+	 */
+	private void displayVersionProperties() {
+		String versionNumber = StringUtils.EMPTY;
+		String versionDate = StringUtils.EMPTY;
+		try {
+			// Gets version number
+			File jadFile = new File(desktop.getWebApp()
+					.getResource("/jar/nogps/Athan.jad").getFile());
+			
+			versionNumber = getVersionNumber(jadFile);
+			
+			// Gets version date
+			File jarFile = new File(desktop.getWebApp()
+					.getResource("/jar/nogps/Athan.jar").getFile());
+			
+			String format = Labels.getLabel("current.releaseDate.format");
+			SimpleDateFormat sdf = new SimpleDateFormat(format);
+			versionDate = sdf.format(new Date(jarFile.lastModified()));
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			LOG.log(Level.SEVERE,
+					"Erreur à la récupération des infos sur la version", exc);
+		} finally {
+			lblCurrentVersion.setValue(versionNumber);
+			lblCurrentDate.setValue(versionDate);
+		}
+	}
+
+	/**
+	 * Returne version number from JAD file
+	 * 
+	 * @param jarFile
+	 * @return version number if found, {@link StringUtils#EMTPY} otherwise
+	 */
+	private String getVersionNumber(File jarFile) {
+		
+		String ret = null;
+
+		Scanner scanner = null;
+		
+		try {
+			// Note that FileReader is used, not File, since File is not
+			// Closeable
+			scanner = new Scanner(new FileReader(jarFile));
+
+			// first use a Scanner to get each line
+			while (scanner.hasNextLine() && ret == null) {
+				ret = processLine(scanner.nextLine());
+			}
+		} catch (FileNotFoundException fex) {
+			fex.printStackTrace();
+		} finally {
+			// ensure the underlying stream is always closed
+			// this only has any effect if the item passed to the Scanner
+			// constructor implements Closeable (which it does in this case).
+			scanner.close();
+		}
+
+		return ret == null ? StringUtils.EMPTY : ret;
+	}
+
+	/**
+	 * Overridable method for processing lines in different ways.
+	 * 
+	 * <P>
+	 * This simple default implementation expects simple name-value pairs,
+	 * separated by an ':' sign. Examples of valid input :
+	 * <tt>height : 167cm</tt> <tt>mass =  65kg</tt>
+	 * <tt>disposition : "grumpy"</tt>
+	 * <tt>this is the name : this is the value</tt>
+	 */
+	private String processLine(String aLine) {
+		// use a second Scanner to parse the content of each line
+		Scanner scanner = new Scanner(aLine);
+		scanner.useDelimiter(":");
+		if (scanner.hasNext()) {
+			String name = scanner.next();
+			String value = scanner.next();
+			
+			if (!StringUtils.isEmpty(name) && !StringUtils.isEmpty(value)) {
+				name = StringUtils.trim(name);
+				value = StringUtils.trim(value);
+				
+				if (StringUtils.equalsIgnoreCase(name, MIDLET_VERSION)) {
+					return value;
+				}
+			}
+		}
+		
+		// no need to call scanner.close(), since the source is a String
+		return null;
+	}
+
+	/**
+	 * Display file properties (file sizes,...)
+	 */
+	private void displayFileProperties() {
 		// Fetches size of the files
 		String jadSize = Labels.getLabel("current.fileSize.unknown");
 		String jarSize = Labels.getLabel("current.fileSize.unknown");
 		String unit = " " + Labels.getLabel("current.File.sizeUnit");
 		try {
-			File jadFile = new File(desktop.getWebApp().getResource("/jar/nogps/Athan.jad")
-				.getFile());
-			File jarFile = new File(desktop.getWebApp().getResource("/jar/nogps/Athan.jar")
-					.getFile());
+			File jadFile = new File(desktop.getWebApp()
+					.getResource("/jar/nogps/Athan.jad").getFile());
+			File jarFile = new File(desktop.getWebApp()
+					.getResource("/jar/nogps/Athan.jar").getFile());
 			jadSize = Long.toString(jadFile.length());
 			jarSize = Long.toString(jarFile.length());
-		} catch(Exception exc) {
+		} catch (Exception exc) {
 			unit = StringUtils.EMPTY;
 			exc.printStackTrace();
-			LOG.log(Level.SEVERE, "Erreur à la récupération de la taille des fichies", exc);
+			LOG.log(Level.SEVERE,
+					"Erreur à la récupération de la taille des fichiers", exc);
 		} finally {
 			lblJadSize.setValue(jadSize + unit);
 			lblJarSize.setValue(jarSize + unit);
@@ -63,10 +179,12 @@ public class CurrentViewCtrl extends GenericForwardComposer {
 		} catch (FileNotFoundException exc) {
 			Clients.alert(Labels.getLabel("current.fileDownload.error"),
 					StringUtils.EMPTY, Messagebox.ERROR);
-			LOG.log(Level.SEVERE, "Erreur au téléchargement du fichier JAD", exc);
+			LOG.log(Level.SEVERE, "Erreur au téléchargement du fichier JAD",
+					exc);
 		} catch (Exception exc) {
 			exc.printStackTrace();
-			LOG.log(Level.SEVERE, "Erreur au téléchargement du fichier JAD", exc);
+			LOG.log(Level.SEVERE, "Erreur au téléchargement du fichier JAD",
+					exc);
 		}
 	}
 
@@ -77,10 +195,12 @@ public class CurrentViewCtrl extends GenericForwardComposer {
 		} catch (FileNotFoundException exc) {
 			Clients.alert(Labels.getLabel("current.fileDownload.error"),
 					StringUtils.EMPTY, Messagebox.ERROR);
-			LOG.log(Level.SEVERE, "Erreur au téléchargement du fichier JAR", exc);
+			LOG.log(Level.SEVERE, "Erreur au téléchargement du fichier JAR",
+					exc);
 		} catch (Exception exc) {
 			exc.printStackTrace();
-			LOG.log(Level.SEVERE, "Erreur au téléchargement du fichier JAR", exc);
+			LOG.log(Level.SEVERE, "Erreur au téléchargement du fichier JAR",
+					exc);
 		}
 	}
 
