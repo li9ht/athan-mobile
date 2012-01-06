3b652a2f17eda8a6dfdeed6fe73838b365b36630
Makavelikal@gmail.com <Makavelikal@gmail.com@7c86f1f7-d457-8aa9-56d4-cee53fa95fa2>
2012-01-06 22:42:41 +0000

    Gestion des NullPointer si pas de nom de région ni de nom de pays

--- trunk/location-fetcher/LocationFetcher/src/athan/web/LocationException.java
+++ trunk/location-fetcher/LocationFetcher/src/athan/web/LocationException.java
@@ -9,17 +9,18 @@ import java.util.logging.Logger;
  * @author Saad BENBOUZID
  */
 public class LocationException extends Exception {
-	
+
 	private static final long serialVersionUID = 7046087855487936734L;
-	private static final Logger log = Logger.getLogger(LocationException.class.getName());
-	
+	private static final Logger log = Logger.getLogger(LocationException.class
+			.getName());
+
 	private String mMsg;
-	
+
 	public LocationException(String pMsg) {
 		mMsg = pMsg;
-		log.info("A customed exception occured :\n" + pMsg);
+		log.info("A custom exception occured :\n" + pMsg);
 	}
-	
+
 	public String getMsg() {
 		return mMsg;
 	}
