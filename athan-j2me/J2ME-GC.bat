dbf1057ed06d48cf00f6e9cd66fdc072aeae0320
Makavelikal@gmail.com <Makavelikal@gmail.com@7c86f1f7-d457-8aa9-56d4-cee53fa95fa2>
2012-12-18 00:20:03 +0000

    Prise en compte de la nouvelle version sur google code

--- athan-j2me/J2ME-GC.bat
+++ athan-j2me/J2ME-GC.bat
@@ -1,2 +1,2 @@
 REM "c:\Samsung_SDK_111\bin\emulator.exe" -Xjam
-"c:\Samsung_SDK_111\bin\emulator.exe" -Xautotest:"http://athan-mobile.googlecode.com/files/Athan-0.3.9.jad"
+"c:\Samsung_SDK_111\bin\emulator.exe" -Xautotest:"http://athan-mobile.googlecode.com/files/Athan-0.4.0.jad"
