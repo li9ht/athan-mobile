a63f46792afc220b907ac85daf698e9749aec943
Makavelikal@gmail.com <Makavelikal@gmail.com@7c86f1f7-d457-8aa9-56d4-cee53fa95fa2>
2015-02-06 15:04:49 +0000

    - Migration du projet sous Maven - Upgrade recaptcha pour prendre en compte les nouvelles URLs de google

---
+++ athan-web/athan-mobile/src/main/java/com/athan/mobile/controls/about/ContributorViewCtrl.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.athan.mobile.controls.about;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+
+/**
+ * Contributor Controler
+ * 
+ * @author Saad BENBOUZID
+ */
+public class ContributorViewCtrl extends GenericForwardComposer {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+	}
+}
---
+++ athan-web/athan-mobile/src/main/java/com/athan/mobile/controls/news/NextfeaturesViewCtrl.java
@@ -0,0 +1,19 @@
+package com.athan.mobile.controls.news;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+
+/**
+ * Nextfeatures Controller
+ * 
+ * @author Saad BENBOUZID
+ */
+public class NextfeaturesViewCtrl extends GenericForwardComposer {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+	}
+}
